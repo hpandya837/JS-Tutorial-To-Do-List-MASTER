6a739bcc82b4e3395e2daae161b547d612026f14
Hpandya837 <callnirav1234@gmail.com>
2024-05-16 14:47:15 -0400

    add to do pt 1

--- js/app.js
+++ js/app.js
@@ -7,4 +7,20 @@ const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 
 const input = document.getElementById("input");
+function addToDo(toDo){
+const text =  <li class="item">
+                    <i class="co fa fa-circle-thin" job="complete"></i>
+                    <p class="text">${toDo}</p>
+                    <i class="de fa fa-trash-o" job="delete"></i>
+                </li>
 
+const position = "beforeend";
+
+list.insertAdjacentHTML(position,text)
+}
+
+document.addEventListener("keyup",function(event){
+
+
+
+})
